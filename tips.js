489df06172eba16fda6727144c10234b498395a3
Wayne <dev.wayne.ix@gmail.com>
2014-06-26 16:32:14 +0800

    add defaults

--- tips.js
+++ tips.js
@@ -1,29 +1,34 @@
 (function($) {
 
-	$.showTips = function(info,options) {
-		var defaults = {
-			time: 2000,
-			width:300,
-			font_size: 14
-		};
+	$.extend({
 
-		var opts = $.extend(defaults, options);
+		showTips: function(info, options) {
+			
+			var opts = $.extend({},$.showTips.defaults, options);
 
-		func.showOverlay(0);
+			func.showOverlay(0);
 
-		var tips = $('<p class="ajax-tips weixin-sharex-tips" style="font-size:'+opts.font_size+'px;text-align:center;background-color:rgba(0,0,0,0.8);position:fixed;width:'+opts.width+'px;padding:40px 10px;-webkit-border-radius:4px;-moz-border-radius:4px;border-radius:4px;color:#fff;line-height:180%;">' + info + '</p>').appendTo('body');
+			var tips = $('<p style="font-size:' + opts.font_size + 'px;text-align:center;background-color:rgba(0,0,0,1);z-index:1000;position:fixed;width:' + opts.width + 'px;padding:40px 10px;-webkit-border-radius:4px;-moz-border-radius:4px;border-radius:4px;color:#fff;line-height:180%;">' + info + '</p>').appendTo('body');
 
-		func.centershow(tips);
+			func.centershow(tips);
 
-		setTimeout(function() {
-			tips.remove();
-			func.showOverlay(1);
-		}, opts.time);
+			setTimeout(function() {
+				tips.remove();
+				func.showOverlay(1);
+			}, opts.time);
+		}
+	});
+
+	$.showTips.defaults = {
+				time: 2000,
+				width: 300,
+				font_size: 14
 	};
 
+
 	var func = {
-		showOverlay: function (clear,opts) {
-			var overlay = $('<div id="J_overlay" class="dialogue_overlay" style="position:absolute;left:0;top:0;background:#000;_filter:alpha(opacity=50);z-index:50000;"></div>');
+		showOverlay: function(clear, opts) {
+			var overlay = $('<div style="position:absolute;left:0;top:0;background:#000;_filter:alpha(opacity=50);z-index:999;"></div>');
 
 			if (!clear) {
 				overlay.appendTo($('body')).css({
@@ -35,7 +40,7 @@
 				$('#J_overlay').remove();
 			}
 		},
-		centershow: function (divName) {
+		centershow: function(divName) {
 			var top = ($(window).height() - $(divName).height()) / 2;
 			var left = ($(window).width() - $(divName).width()) / 2;
 			var scrollTop = $(document).scrollTop();
