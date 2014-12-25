c86d88f102355279d6b80737ca5078faa899a529
Wayne <dev.wayne.ix@gmail.com>
2014-12-25 15:40:54 +0800

    addgetsize

--- toast.js
+++ toast.js
@@ -1,55 +1,101 @@
-		(function($) {
+(function($) {
 
-			$.toast= function(info, options) {
+    $.toast = function(info, options) {
 
-					var opts = $.extend({}, $.toast.defaults, options);
+        var opts = $.extend({}, $.toast.defaults, options);
 
-					func.showOverlay(0);
+        func.showOverlay(0);
 
-					var tips = $('<p style="font-size:' + opts.font_size + 'px;text-align:center;vertical-align:middle;background-color:rgba(0,0,0,1);z-index:1000;position:fixed;width:' + opts.width + 'px;height:' + opts.height + 'px;-webkit-border-radius:4px;-moz-border-radius:4px;border-radius:4px;color:#fff;line-height:' + opts.height + 'px;">' + info + '</p>').appendTo('body');
+        var tips = $('<p style="font-size:' + opts.font_size + 'px;text-align:center;vertical-align:middle;background-color:rgba(0,0,0,1);z-index:1000;position:fixed;width:' + opts.width + 'px;height:' + opts.height + 'px;-webkit-border-radius:4px;-moz-border-radius:4px;border-radius:4px;color:#fff;line-height:' + opts.height + 'px;">' + info + '</p>').appendTo('body');
 
-					func.centershow(tips);
+        func.centershow(tips);
 
-					setTimeout(function() {
-						tips.remove();
-						func.showOverlay(1);
-					}, opts.time);
-				}
-			
-
-			$.toast.defaults = {
-				time: 2000,
-				width: 'auto',
-				height: 'auto',
-				font_size: 14
-			};
+        setTimeout(function() {
+            tips.remove();
+            func.showOverlay(1);
+        }, opts.time);
+    }
 
 
-			var func = {
-				showOverlay: function(clear, opts) {
-					var overlay = $('<div id="J_overlay" style="position:absolute;left:0;top:0;background:#000;_filter:alpha(opacity=50);z-index:999;"></div>');
+    $.toast.defaults = {
+        time: 2000,
+        width: 'auto',
+        height: 'auto',
+        font_size: 14
+    };
 
-					if (!clear) {
-						overlay.appendTo($('body')).css({
-							'width': $(window).width(),
-							'height': $(window).height(),
-							'opacity': 0.5
-						});
-					} else {
-						$('#J_overlay').remove();
-					}
-				},
-				centershow: function(divName) {
-					var top = ($(window).height() - $(divName).height()) / 2;
-					var left = ($(window).width() - $(divName).width()) / 2;
-					var scrollTop = $(document).scrollTop() || 0;
-					var scrollLeft = $(document).scrollLeft() || 0;
-					$(divName).css({
-						position: 'absolute',
-						'top': top + scrollTop,
-						'left': left + scrollLeft
-					}).show();
-					//$('#_overlay_').show();
-				}
-			}
-		})(jQuery);
+
+    var func = {
+        showOverlay: function(clear, opts) {
+            var overlay = $('<div id="J_overlay" style="position:absolute;left:0;top:0;background:#000;_filter:alpha(opacity=50);z-index:999;"></div>');
+
+            if (!clear) {
+                overlay.appendTo($('body')).css({
+                    'width': func.getPageSize()[1],
+                    'height': func.getPageSize()[0],
+                    'opacity': 0.5
+                });
+            } else {
+                $('#J_overlay').remove();
+            }
+        },
+        centershow: function(divName) {
+            var top = ($(window).height() - $(divName).height()) / 2;
+            var left = ($(window).width() - $(divName).width()) / 2;
+            var scrollTop = $(document).scrollTop() || 0;
+            var scrollLeft = $(document).scrollLeft() || 0;
+            $(divName).css({
+                position: 'absolute',
+                'top': top + scrollTop,
+                'left': left + scrollLeft
+            }).show();
+        },
+        getPageSize: function() {
+            var xScroll, yScroll;
+            if (window.innerHeight && window.scrollMaxY) {
+                xScroll = window.innerWidth + window.scrollMaxX;
+                yScroll = window.innerHeight + window.scrollMaxY;
+            } else {
+                if (document.body.scrollHeight > document.body.offsetHeight) { // all but Explorer Mac    
+                    xScroll = document.body.scrollWidth;
+                    yScroll = document.body.scrollHeight;
+                } else { // Explorer Mac...would also work in Explorer 6 Strict, Mozilla and Safari    
+                    xScroll = document.body.offsetWidth;
+                    yScroll = document.body.offsetHeight;
+                }
+            }
+            var windowWidth, windowHeight;
+            if (self.innerHeight) { // all except Explorer    
+                if (document.documentElement.clientWidth) {
+                    windowWidth = document.documentElement.clientWidth;
+                } else {
+                    windowWidth = self.innerWidth;
+                }
+                windowHeight = self.innerHeight;
+            } else {
+                if (document.documentElement && document.documentElement.clientHeight) { // Explorer 6 Strict Mode    
+                    windowWidth = document.documentElement.clientWidth;
+                    windowHeight = document.documentElement.clientHeight;
+                } else {
+                    if (document.body) { // other Explorers    
+                        windowWidth = document.body.clientWidth;
+                        windowHeight = document.body.clientHeight;
+                    }
+                }
+            }
+            if (yScroll < windowHeight) {
+                pageHeight = windowHeight;
+            } else {
+                pageHeight = yScroll;
+            h total width less then width of the viewport    
+            if (xScroll < windowWidth) {
+                pageWidth = xScroll;
+            } else {
+                pageWidth = windowWidth;
+            }
+            arrayPageSize = new Array(pageHeight, pageWidth, windowWidth, windowHeight);
+            return arrayPageSize;
+        }
+
+    }
+})($);
